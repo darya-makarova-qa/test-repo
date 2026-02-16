735cf216f5e4bc63e5b0638cbe7d883fd6bbb878
darya-makarova-qa <mdara4257@gmail.com>
2026-02-16 10:00:06 +0200

    Update test_products.py

--- tests/test_products.py
+++ tests/test_products.py
@@ -2,4 +2,4 @@ from playwright.sync_api import expect
 def test_all_products(page):
     page.goto("https://automationexercise.com/")
     page.get_by_role("link", name = "Products").click()
-    expect(page.get_by_text("All products").to_be_visible()()
+    expect(page.get_by_text("All products").to_be_visible()
